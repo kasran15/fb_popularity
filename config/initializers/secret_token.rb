d0df5b98baf00ac5d2c2768042b8b7827c50d295
Kazz <kasran15@gmail.com>
2012-09-25 23:59:12 -0400

    directory restructure

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FbPopularity::Application.config.secret_token = 'f17d9531ffeb43e63e9581dca47a6925c8370afed8614c8f39f5eb29ae32f496fb95428d7a6c9b2d9ac5a0eaab2a28c7dfb00e59fec45c6882dbba1050e5d2a4'
